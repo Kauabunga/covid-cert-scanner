69bf4304a1954fd53c00580bbaffe9cfbf8d221a
Carson Bruce <carson.bruce@middleware.co.nz>
2021-11-10 09:39:52 +1300

    Tidy up appbar component

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,23 +6,14 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
 
 import AppBarDrawer from "./AppBarDrawer";
 
 export default function MenuAppBar({ children }) {
-  const [auth, setAuth] = React.useState(false);
   const [drawer, togglerDrawer] = React.useState();
-  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const toggleMenu = () => {
     togglerDrawer((currentState) => !currentState);
-  };
-
-  const handleMenu = (event) => {
-    togglerDrawer(true);
   };
 
   const handleClose = () => {
@@ -46,52 +37,6 @@ export default function MenuAppBar({ children }) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Scanner
           </Typography>
-
-          {!auth && (
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          )}
-
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
         </Toolbar>
       </AppBar>
 
